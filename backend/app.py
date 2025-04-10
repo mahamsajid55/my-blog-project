ce33a02a1acd16074f5ba42b4e86b507b91dbf1a
maham <mahamsajid450@gmail.com>
2025-04-10 21:00:31 +0200

    cambios

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 import os
 
-# Set the templates folder path explicitly
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'),
+                      static_folder=os.path.join(os.path.dirname(__file__), '..', 'static'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')  
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
