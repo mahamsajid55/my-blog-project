1ff8c01ffc9ab3a476852a3dfdb503390c272aa1
maham <mahamsajid450@gmail.com>
2025-04-10 12:36:11 +0200

    cambio

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '.
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Now Flask should find index.html
+    return render_template('index.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
